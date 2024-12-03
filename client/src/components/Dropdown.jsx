810e39385a7fc41d3cade8979ea81312f9241427
Manojkamriya <kamriyamanoj45@gmail.com>
2024-12-03 15:31:35 +0530

    Navbar iOS CSS issues resolved and dropdowm improved

--- client/src/components/Dropdown.jsx
+++ client/src/components/Dropdown.jsx
@@ -2,31 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./dropdown.css";
 
-function Dropdown({ category, topProducts, featured, imageSrc, launchTitle,setActiveDropdown }) {
+function Dropdown({
+  category,
+  topProducts,
+  featured,
+  imageSrc,
+  launchTitle,
+  setActiveDropdown,
+}) {
   return (
     <div className="dropdowm-container">
       <div className="dropdown">
-        <div className="sub-menu">
+        <div className="sub-menu-categories">
           <h5>TOP PRODUCTS</h5>
           <ul>
-            {topProducts.map((product, index) => (
-              <li key={index} onClick={()=>setActiveDropdown(null)}>
-                <Link to={`/${category}/${product.toLowerCase()}`}>{product}</Link>
+            {topProducts.slice(0, 25).map((product, index) => (
+              <li key={index} onClick={() => setActiveDropdown(null)}>
+                <Link to={`/${category}/${product.toLowerCase()}`}>
+                  {product}
+                </Link>
               </li>
             ))}
+            <ul></ul>
           </ul>
         </div>
-        <div className="sub-menu">
-          <h5>FEATURED</h5>
-          <ul>
-            {featured.map((item, index) => (
-              <li key={index} onClick={()=>setActiveDropdown(null)}>
-                <Link to={`/${category}`}>{item}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="sub-menu">
+        <div className="sub-menu-image">
           <img className="submenu-image" src={imageSrc} alt={launchTitle} />
           <h4>{launchTitle}</h4>
         </div>
