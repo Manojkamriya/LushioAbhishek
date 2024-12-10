c9cd77bb6add8c5a050a704db9622e1a56b546dc
Abhishek Verma <abhishekaverma20@gmail.com>
2024-12-10 18:01:10 +0530

    login redirect bug resolved

--- client/src/auth/emailAuth.js
+++ client/src/auth/emailAuth.js
@@ -2,23 +2,25 @@ import { auth, db } from "../firebaseConfig";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, sendSignInLinkToEmail } from "firebase/auth";
 import { doc, setDoc, updateDoc, getDoc } from "firebase/firestore";
 
+// Determine the frontend URL dynamically from environment variables
+const FRONTEND_URL = process.env.REACT_APP_FRONTEND_URL;
+
 const handleEmailSignUp = async (email, password, referralCode) => {
   try {
     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
     const user = userCredential.user;
 
-    // Ensure referralCode is either a string value or an empty string
-    const finalReferralCode = referralCode ? referralCode : "";
+    const finalReferralCode = referralCode || "";
 
     await setDoc(doc(db, "users", user.uid), {
       email: user.email,
       createdAt: new Date(),
-      referralCode: finalReferralCode, // Pass referralCode or empty string
-      lastSignInTime: new Date()  // Set lastSignInTime for new user
+      referralCode: finalReferralCode,
+      lastSignInTime: new Date(),
     });
 
     const actionCodeSettings = {
-      url: `${process.env.REACT_APP_FRONTEND_URL}/finishSignIn?cartId=1234`,
+      url: `${FRONTEND_URL}/finishSignIn?cartId=1234`,
       handleCodeInApp: true,
     };
     await sendSignInLinkToEmail(auth, email, actionCodeSettings);
@@ -27,7 +29,6 @@ const handleEmailSignUp = async (email, password, referralCode) => {
     window.localStorage.setItem("emailForSignIn", email);
 
     console.log(user);
-    
     return user;
 
   } catch (error) {
@@ -41,14 +42,12 @@ const handleEmailLogin = async (email, password) => {
     const result = await signInWithEmailAndPassword(auth, email, password);
     const user = result.user;
 
-    // Reference to the user document in Firestore
     const userDoc = doc(db, "users", user.uid);
     const userSnapshot = await getDoc(userDoc);
 
     if (userSnapshot.exists()) {
-      // Update lastSignInTime if the user already exists
       await updateDoc(userDoc, {
-        lastSignInTime: new Date()
+        lastSignInTime: new Date(),
       });
     }
 
@@ -63,7 +62,7 @@ const handleEmailLogin = async (email, password) => {
 const sendEmailSignInLink = async (email) => {
   try {
     const actionCodeSettings = {
-      url: `${process.env.REACT_APP_FRONTEND_URL}/finishSignIn`,
+      url: `${FRONTEND_URL}/finishSignIn`,
       handleCodeInApp: true,
     };
     await sendSignInLinkToEmail(auth, email, actionCodeSettings);
